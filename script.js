2cd12e06f026efab0e6fc65a609da138b00a2217
Chowdhury Nur <chynur380@gmail.com>
2026-08-08 11:42:59 +0600

    Updated website with new sections and styling

--- script.js
+++ script.js
@@ -7,12 +7,8 @@ function scrollToSection(id) {
 document.getElementById('contactForm').addEventListener('submit', function(e) {
   e.preventDefault();
   const name = document.getElementById('name').value;
-  const email = document.getElementById('email').value;
-  const message = document.getElementById('message').value;
-
   document.getElementById('formResponse').innerText =
     `Thank you, ${name}! Your message has been received.`;
-  
-  // Reset form
   this.reset();
 });
+
